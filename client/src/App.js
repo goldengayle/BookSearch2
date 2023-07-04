519f1cb97bed4c486e9d7f2e7f88c92aae9fed72
goldie <codegayle@gmail.com>
2023-07-04 15:07:04 -0400

    search book page renders

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
+
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
+import {setContext} from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
-})
+});
 
 function App() {
   return (
@@ -17,8 +35,8 @@ function App() {
       <>
         <Navbar />
         <Routes>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
+          <Route exact path='/' element={<SearchBooks/>} />
+          <Route exact path='/saved' element={<SavedBooks/>} />
           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
         </Routes>
       </>
